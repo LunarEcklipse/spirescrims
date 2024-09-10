9d6149e26a63c75af1e16e12fb155b493f7752ef
LunarEcklipse <kadenduncanmatis@gmail.com>
2024-09-10 10:13:29 -0600

    More MMR stuff

--- bot/lib/obj/scrim_match.py
+++ bot/lib/obj/scrim_match.py
@@ -1,2 +1,20 @@
+from typing import Union, List, Dict, Tuple
+
+from lib.obj.scrim_format import ScrimFormat
+from lib.obj.scrim_user import ScrimUser
+from lib.obj.scrim_team import ScrimTeam
+from lib.scrim_mmr_calculation import ScrimMMR
+
 class ScrimMatch:
-    pass
+    format: ScrimFormat
+    groups: Union[List[ScrimUser], List[ScrimTeam]]
+    winner_score: Union[int, None]
+
+    def calculate_mmr_change_range(self, constant: int = 32) -> Dict[Union[ScrimUser, ScrimTeam], Tuple[int, int]]:
+        '''
+        Calculate the range of MMR changes for each group in the match.
+        '''
+        mmr_changes = {}
+        for group in self.groups:
+            mmr_changes[group] = (ScrimMMR.calculate_maximum_mmr_loss, ScrimMMR.calculate_maximum_mmr_gain)
+        return mmr_changes
--- bot/lib/scrim_mmr_calculation.py
+++ bot/lib/scrim_mmr_calculation.py
@@ -1,11 +1,69 @@
-from typing import Union
+from typing import Union, List
 from lib.obj.scrim_user import ScrimUser
 from lib.obj.scrim_team import ScrimTeam
 
+class ScrimGroupResult:
+    group: Union[ScrimUser, ScrimTeam]
+    placement: int
+    total_score: int
+
+    def __init__(self, group: Union[ScrimUser, ScrimTeam], placement: int, total_score: int) -> None:
+        self.group = group
+        self.placement = placement
+        self.total_score = total_score
+
 class ScrimMMR:
     @staticmethod
-    def calculate_expected_performance(group_1: Union[ScrimUser, ScrimTeam], group_2: Union[ScrimUser, ScrimTeam]) -> float:
+    def calculate_expected_performance_against_group(group: Union[ScrimUser, ScrimTeam], opposing_group: Union[ScrimUser, ScrimTeam]) -> float:
         '''
         Calculate the expected performance of a group of players against another group of players.
         '''
+        group = group.calculate_group_mmr() if type(group) == ScrimTeam else group.mmr
+        opposing_group = opposing_group.calculate_group_mmr() if type(opposing_group) == ScrimTeam else opposing_group.mmr
+        return 1 / (1 + 10 ** ((opposing_group - group) / 400))
+    
+    @staticmethod
+    def calculate_expected_performance_against_lobby(group: Union[ScrimUser, ScrimTeam], opposing_groups: Union[List[ScrimUser], List[ScrimTeam]]) -> float:
+        '''
+        Calculate the expected performance of a group of players against a lobby of players.
+        '''
+        average_opponent_mmr = 0
+        for opposing_group in opposing_groups:
+            average_opponent_mmr += opposing_group.calculate_group_mmr() if type(opposing_group) == ScrimTeam else opposing_group.mmr
+        average_opponent_mmr //= len(opposing_groups)
+        group = group.calculate_group_mmr() if type(group) == ScrimTeam else group.mmr
+        return 1 / (1 + 10 ** ((average_opponent_mmr - group) / 400))
+    
+    @staticmethod
+    def calculate_mmr_change_against_group(group: Union[ScrimUser, ScrimTeam], match: Union[List[ScrimUser], List[ScrimTeam]], group_score: int, winner_score: int, constant: int = 32) -> float:
+        '''
+        Calculate the mmr change of a group of players against another group of players based on how they placed.
+        '''
         
+        expected_performance = ScrimMMR.calculate_expected_performance_against_group(group, match)
+        actual_performance = group_score / winner_score
+        return constant * (actual_performance - expected_performance)
+    
+    @staticmethod
+    def calculate_new_group_mmr(group: Union[ScrimUser, ScrimTeam], match: Union[List[ScrimUser], List[ScrimTeam]], group_score: int, winner_score: int, constant: int = 32) -> int:
+        '''
+        Calculate the new MMR of a group of players after a match.
+        '''
+        current_mmr = group.calculate_group_mmr() if type(group) == ScrimTeam else group.mmr
+        return current_mmr + ScrimMMR.calculate_mmr_change_against_group(group, match, group_score, winner_score, constant)
+    
+    @staticmethod
+    def calculate_maximum_mmr_gain(group: Union[ScrimUser, ScrimTeam], match: Union[List[ScrimUser], List[ScrimTeam]], constant: int = 32) -> int:
+        '''
+        Calculate the maximum MMR a group of players can gain in a match.
+        '''
+        expected_performance = ScrimMMR.calculate_expected_performance_against_group(group, match)
+        return constant * (1 - expected_performance)
+    
+    @staticmethod
+    def calculate_maximum_mmr_loss(group: Union[ScrimUser, ScrimTeam], match: Union[List[ScrimUser], List[ScrimTeam]], constant: int = 32) -> int:
+        '''
+        Calculate the maximum MMR a group of players can lose in a match.
+        '''
+        expected_performance = ScrimMMR.calculate_expected_performance_against_group(group, match)
+        return constant * (0 - expected_performance)
