eded1dada56acffe537d386029dcecdcaeff77ef
LunarEcklipse <kadenduncanmatis@gmail.com>
2024-09-12 09:08:32 -0600

    Built whois command

--- bot/lib/scrim_debugcommands.py
+++ bot/lib/scrim_debugcommands.py
@@ -14,8 +14,8 @@ class ScrimDebugCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.debug_channels = ScrimDebugChannels.get_debug_channels()
-        print(self.debug_channels)
 
+    # Set Sweet ID
     @commands.command(name="setsweetid")
     async def connect_sweet_id(self, ctx: discord.ApplicationContext, discord_id: int, sweet_id: str):
         print("Command called!")
@@ -39,3 +39,27 @@ class ScrimDebugCommands(commands.Cog):
             user = ScrimUserData.get_user_by_discord_id(discord_id)
         ScrimUserData.connect_sweet_to_id(user.scrim_id, sweet_id)
         await ctx.send(f"Connected Sweet ID: `{sweet_id}` to Discord ID `{discord_id}`", reply=True)
+
+    # Who is
+    @commands.command(name="whois")
+    async def whois(self, ctx: discord.ApplicationContext, discord_id: int):
+        # If not in a debug channel, ignore
+        if ctx.channel.id not in self.debug_channels:
+            return
+        user: ScrimUser = ScrimUserData.get_user_by_discord_id(discord_id)
+        if user is None:
+            await ctx.send(f"Could not find user with Discord ID: `{discord_id}`", reply=True)
+            return
+        discord_user: Union[discord.User, discord.Member] = await self.bot.fetch_user(discord_id)
+        # Construct the embed
+        emb = discord.Embed(title=discord_user.name, color=discord.Color.green())
+        emb.set_thumbnail(url=discord_user.display_avatar.url)
+        emb.add_field(name="Spire Scrims ID", value=user.scrim_id, inline=False)
+        if user.sweet_id is not None:
+            emb.add_field(name="Sweet ID", value=user.sweet_id, inline=False)
+        if user.twitch_id is not None:
+            emb.add_field(name="Twitch ID", value=user.twitch_id, inline=False)
+        emb.set_footer(text=f"Discord ID: {str(discord_user.id)}")
+        await ctx.send(content=None, embed=emb, reference=ctx.message)
+
+    
